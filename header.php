3695ddf9fb276a05b1520e47278664c30bd03b64
Ubiratã Carvalho <birah.carvalho@gmail.com>
2018-04-04 22:07:30 -0300

    Removi a propriedade fixed do menu

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 </head>
 <body <?php body_class(); ?>  >
 <header>
-    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <a class="navbar-brand" href="#">WPBaseTheme</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
